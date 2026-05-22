01d8d80df27e5c5a57eff8e7a3e73b5c092c334b
jyoti1597 <jyotichilwal1@gmail.com>
2026-05-22 17:02:50 +0530

    login page ui part done

---
+++ panel/login/login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Dashboard UI</title>
+        <link rel="stylesheet" href="../assets/css/style.css">
+        <link rel="stylesheet" href="../assets/css/media.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+
+    <body>
+        <!-- Main Content -->
+        <div class="loginContainer">
+            <div class="loginCard">
+                <img src="../assets/images/logo_image.png" alt="Logo">
+                <h2>Login</h2>
+                <form action="#" method="POST">
+                    <input type="text" name="username" placeholder="Username" required>
+                    <input type="password" name="password" placeholder="Password" required>
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
