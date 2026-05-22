6e96b7b8e5a8292e351d9157e7b9c033d7f94381
jyoti1597 <jyotichilwal1@gmail.com>
2026-05-22 17:07:36 +0530

    otp verification and reset password page ui done

--- panel/login/otp_verification.php
+++ panel/login/otp_verification.php
@@ -14,13 +14,16 @@
         <div class="loginContainer">
             <div class="loginCard">
                 <img src="../assets/images/logo_image.png" alt="Logo">
-                <h2>Reset Password</h2>
+                <h2>OTP Verification</h2>
                 <form action="#" method="POST">
+                    <input type="email" name="email" placeholder="Email" readonly>
                     <div class="justify-content-center">
-                        <input type="password" name="new_password" placeholder="New Password" required>
-                        <input type="password" name="confirm_password" placeholder="Confirm Password" required>
+                        <input type="otp" name="otp" placeholder="OTP" required>
                     </div>
-                    <button type="submit">Reset Password</button>
+                    <div class="justify-content-center">
+                        <a href="reset_password.php">Resend OTP</a>
+                        <button type="submit">Verify OTP</button>
+                    </div>
                 </form>
             </div>
         </div>
