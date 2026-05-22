67ab97551bdfd11f71a6294bc688890cc0f27214
jyoti1597 <jyotichilwal1@gmail.com>
2026-05-22 17:10:32 +0530

    modify otp page

--- panel/login/otp_verification.php
+++ panel/login/otp_verification.php
@@ -16,11 +16,13 @@
                 <img src="../assets/images/logo_image.png" alt="Logo">
                 <h2>OTP Verification</h2>
                 <form action="#" method="POST">
-                    <input type="email" name="email" placeholder="Email" readonly>
-                    <div class="justify-content-center">
+                    <div class="row">
+                        <p>An OTP has been sent to your email address. Please enter the OTP below to verify your account.<b>user@example.com</b></p>
+                    </div>
+                    <div class="row inputGroup">
                         <input type="otp" name="otp" placeholder="OTP" required>
                     </div>
-                    <div class="justify-content-center">
+                    <div class="row">
                         <a href="reset_password.php">Resend OTP</a>
                         <button type="submit">Verify OTP</button>
                     </div>
